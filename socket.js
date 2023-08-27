8ac64b19c139364c9f70eb0b7fc9e4090c169b05
LuizAugustoGrein <>
2023-08-27 17:49:45 -0300

    hangup-and-webcam-control

--- socket.js
+++ socket.js
@@ -2,58 +2,87 @@ const { Server } = require("socket.io");
 let IO;
 
 module.exports.initIO = (httpServer) => {
-  IO = new Server(httpServer);
+    IO = new Server(httpServer);
 
-  IO.use((socket, next) => {
-    if (socket.handshake.query) {
-      let callerId = socket.handshake.query.callerId;
-      socket.user = callerId;
-      next();
-    }
-  });
-
-  IO.on("connection", (socket) => {
-    console.log(socket.user, "Connected");
-    socket.join(socket.user);
-
-    socket.on("call", (data) => {
-      let calleeId = data.calleeId;
-      let rtcMessage = data.rtcMessage;
-
-      socket.to(calleeId).emit("newCall", {
-        callerId: socket.user,
-        rtcMessage: rtcMessage,
-      });
+    IO.use((socket, next) => {
+        if (socket.handshake.query) {
+            let callerId = socket.handshake.query.callerId;
+            console.log('handshake: ' + callerId);
+            socket.user = callerId;
+            next();
+        }
     });
 
-    socket.on("answerCall", (data) => {
-      let callerId = data.callerId;
-      rtcMessage = data.rtcMessage;
+    IO.on("connection", (socket) => {
+        socket.join(socket.user);
 
-      socket.to(callerId).emit("callAnswered", {
-        callee: socket.user,
-        rtcMessage: rtcMessage,
-      });
+        socket.on("call", (data) => {
+            console.log('oncall: ' + JSON.stringify(data));
+            let calleeId = data.calleeId;
+            let rtcMessage = data.rtcMessage;
+
+            socket.to(calleeId).emit("newCall", {
+                callerId: socket.user,
+                rtcMessage: rtcMessage,
+            });
+        });
+
+        socket.on("answerCall", (data) => {
+            console.log('onanswerCall: ' + JSON.stringify(data));
+            let callerId = data.callerId;
+            rtcMessage = data.rtcMessage;
+
+            socket.to(callerId).emit("callAnswered", {
+                callee: socket.user,
+                rtcMessage: rtcMessage,
+            });
+        });
+
+        socket.on("ICEcandidate", (data) => {
+            console.log('onICEcandidate: ' + JSON.stringify(data));
+            let calleeId = data.calleeId;
+            let rtcMessage = data.rtcMessage;
+
+            socket.to(calleeId).emit("ICEcandidate", {
+                sender: socket.user,
+                rtcMessage: rtcMessage,
+            });
+        });
+
+        socket.on("hangup", (data) => {
+            console.log('hangup: ' + JSON.stringify(data));
+            let calleeId = data.calleeId;
+
+            socket.to(calleeId).emit("hangupCall", {
+                callerId: socket.user
+            });
+        });
+
+        socket.on("cameraOff", (data) => {
+            console.log('cameraOff');
+            let calleeId = data.calleeId;
+
+            socket.to(calleeId).emit("turnCameraOff", {
+                callerId: socket.user
+            });
+        });
+
+        socket.on("cameraOn", (data) => {
+            console.log('cameraOn');
+
+            let calleeId = data.calleeId;
+
+            socket.to(calleeId).emit("turnCameraOn", {
+                callerId: socket.user
+            });
+        });
     });
-
-    socket.on("ICEcandidate", (data) => {
-      console.log("ICEcandidate data.calleeId", data.calleeId);
-      let calleeId = data.calleeId;
-      let rtcMessage = data.rtcMessage;
-      console.log("socket.user emit", socket.user);
-
-      socket.to(calleeId).emit("ICEcandidate", {
-        sender: socket.user,
-        rtcMessage: rtcMessage,
-      });
-    });
-  });
 };
 
 module.exports.getIO = () => {
-  if (!IO) {
-    throw Error("IO not initilized.");
-  } else {
-    return IO;
-  }
+    if (!IO) {
+        throw Error("IO not initilized.");
+    } else {
+        return IO;
+    }
 };
